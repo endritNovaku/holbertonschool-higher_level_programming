96acfc8fdfac8b2957ac27cb1ed95eddf928bed8
endritNovaku <ennald@outlook.com>
2022-04-26 06:19:27 -0700

    101-remove_char_at.py

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    string = ""
+    j = 0
+    for i in str:
+        if j != n:
+            string = string + i
+            j = j + 1
+        else:
+            j = j + 1
+
+    return string
