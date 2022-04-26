66f4105575d9093d18253e7652aa5784a86b1ad5
endritNovaku <ennald@outlook.com>
2022-04-26 05:26:51 -0700

    9-print_last_digit.py

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number >= 0:
+        n = number % 10
+    else:
+        n = (number - number - number) % 10
+
+    print(n, end="")
+    return n
