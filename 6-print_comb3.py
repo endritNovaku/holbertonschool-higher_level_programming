c752b85f02b7ab05a1347d6a78785b260031282a
endritNovaku <ennald@outlook.com>
2022-04-26 03:08:53 -0700

    6 tasks

---
+++ 6-print_comb3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+for i in range(10):
+
+    for j in range(10):
+        if j <= i:
+            continue
+        elif i != 8 or j != 9:
+            print("{}{}".format(i,j), end=", ")
+        else:
+            print("{}{}".format(i,j))
