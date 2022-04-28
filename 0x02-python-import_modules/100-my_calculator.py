f99519428a04ed4b88c7e0713fc0125270934f02
endritNovaku <ennald@outlook.com>
2022-04-28 06:24:16 -0700

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
 
     op1 = int(argv[1])
     op2 = argv[2]
-    op3 = int(argv[3]) 
+    op3 = int(argv[3])
     if op2 == "+":
         print(op1, op2, op3, "=", add(op1, op3))
     if op2 == "-":
