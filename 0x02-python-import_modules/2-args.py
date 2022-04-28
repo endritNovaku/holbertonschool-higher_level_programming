12e36f2c83c8219c875d3aee756f039a2a334d79
endritNovaku <ennald@outlook.com>
2022-04-28 03:15:48 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import sys
-length = len(sys.argv)
-if length == 1:
-    print("0 argument.")
-else:
-    print("{} argument:".format(length - 1))
-    for i in range(1, length):
-        print(i, sys.argv[i])
+def main():
+    length = len(sys.argv)
+    if length == 1:
+        print("0 argument.")
+    else:
+        print("{} argument:".format(length - 1))
+        for i in range(1, length):
+            print(i, sys.argv[i])
