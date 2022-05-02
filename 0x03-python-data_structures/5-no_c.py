109bf3524f558719e5a168af8ffb4715e9403ed3
endritNovaku <ennald@outlook.com>
2022-05-02 07:20:57 -0700

    remove characters c or C from a string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-#remove c or C from a string
+# remove c or C from a string
 def no_c(my_string):
     new_list = []
-    #turn the string to list and remove c characters
+    # turn the string to list and remove c characters
     for i in range(0, len(my_string)):
         if my_string[i] != 'c' and my_string[i] != 'C':
             new_list.append(my_string[i])
-    #convert the list back to a string and return the new_string
+    # convert the list back to a string and return the new_string
     new_string = ''.join(map(str, new_list))
     return new_string
