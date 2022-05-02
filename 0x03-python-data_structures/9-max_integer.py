6637d458b2683c636123e6cbf4ed8ae57857519e
endritNovaku <ennald@outlook.com>
2022-05-02 11:59:16 -0700

    fixing bug

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     # return the biggest number from the array
-    biggest_num = 0
     if not my_list:
         return None
-    for i in range(len(my_list)):
-        if my_list[i] > biggest_num:
-            biggest_num = my_list[i]
-    return(biggest_num)
+    my_list.sort()
+    return(my_list[-1])
