85998feca1d151f256f795998081a8498756a97c
endritNovaku <ennald@outlook.com>
2022-05-02 11:50:03 -0700

    fixing bug

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,6 @@
 def multiple_returns(sentence):
     # return the length of the sentence and the first character
     # if sentence is empty first character should be equal to None
-    if len(sentence) != 0:
-        return((len(sentence), sentence[0]))
-    else:
-        return((len(sentence, None))
+    if len(sentence) == 0:
+        return((len(sentence), None))
+    return((len(sentence), sentence[0]))
