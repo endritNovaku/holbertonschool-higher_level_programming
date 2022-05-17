88aaa147d2b4dc353d32fa98dfd73f551bcdc346
endritNovaku <ennald@outlook.com>
2022-05-17 07:02:59 -0700

    5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,14 +1,22 @@
+#!/usr/bin/python3
+"""create squares"""
+
+
 class Square:
+    """inicialize size"""
     def __init__(self, size=0):
         self.__size = int(size)
 
+    """return size"""
     @property
     def size(self):
         return (self.__size)
 
+    """return the square"""
     def area(self):
         return (self.__size * self.__size)
 
+    """size"""
     @size.setter
     def size(self, value):
         self.__size = value
@@ -17,6 +25,7 @@ class Square:
         if self.__size < 0:
             raise ValueError("size must be >= 0")
 
+    """print the square"""
     def my_print(self):
         for i in range(self.__size):
             for j in range(self.__size):
