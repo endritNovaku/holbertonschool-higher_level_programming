1ec978e4d4e93483fcec8f5c73fa1ceec158c710
endritNovaku <ennald@outlook.com>
2022-05-17 06:53:50 -0700

    2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,10 +3,10 @@
 
 
 class Square:
-    """Square check for errors"""
+    """Check for errors"""
     def __init__(self, size=0):
         self.__size = size
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
-            raise ValueError("size must be >=0")
+            raise ValueError("size must be >= 0")
