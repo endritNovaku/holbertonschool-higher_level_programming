171799c6129ab713fc75566e28e49a706614294f
endritNovaku <ennald@outlook.com>
2022-05-17 06:59:39 -0700

    4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
+"""create size function"""
+
+
 class Square:
+    """initialize size"""
     def __init__(self, size=0):
         self.__size = int(size)
 
+    """return size"""
     @property
     def size(self):
         return (self.__size)
 
+    """find root sqr"""
     def area(self):
         return (self.__size * self.__size)
 
+    """add a value"""
     @size.setter
     def size(self, value):
         self.__size = value
