c86c63de5c2e6bb2b40d419e07a87281edfd7801
endritNovaku <ennald@outlook.com>
2022-05-17 06:56:45 -0700

    3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Return the square"""
+
+
 class Square:
+    """Check for errors"""
     def __init__(self, size=0):
         self.__size = int(size)
         if type(self.__size) != int:
@@ -7,5 +11,6 @@ class Square:
         if self.__size < 0:
             raise ValueError("size must be >= 0")
 
+    """Return the square"""
     def area(self):
         return self.__size * self.__size
