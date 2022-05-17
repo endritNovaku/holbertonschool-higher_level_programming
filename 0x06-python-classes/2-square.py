95bbe62ed6729995a8a560233dfa29e218080bfd
endritNovaku <ennald@outlook.com>
2022-05-17 06:47:45 -0700

    2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Square check for errors"""
+
+
 class Square:
+    """Square check for errors"""
     def __init__(self, size=0):
         self.__size = size
         if type(size) != int:
