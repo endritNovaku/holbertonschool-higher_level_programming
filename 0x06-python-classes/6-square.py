19ae116dbe6c7899987c9e04249c664e2db41f9d
endritNovaku <ennald@outlook.com>
2022-05-18 04:51:12 -0700

    fixing error

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -31,7 +31,7 @@ class Square:
     def position(self, value):
         if type(value) != tuple or len(value) != 2 or type(value[0]) != int \
                 or type(value[1]) != int or value[0] < 0 or value[1] < 0:
-            raise TypeError("position must be a tuple of 2 positiove int eger")
+            raise TypeError("position must be a tuple of 2 positiove integer")
         self.__position = value
 
     """return the square"""
