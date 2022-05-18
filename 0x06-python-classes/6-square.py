6cb5451c4997d6f60b7b8028844e17989d7f0272
endritNovaku <ennald@outlook.com>
2022-05-18 04:49:13 -0700

    fixing bugg

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -23,7 +23,7 @@ class Square:
         if type(value) != int:
             raise TypeError("size must be an integer")
         if value < 0:
-            raise ValueError("size must be >=0")
+            raise ValueError("size must be >= 0")
         self.__size = value
 
     """position errors"""
