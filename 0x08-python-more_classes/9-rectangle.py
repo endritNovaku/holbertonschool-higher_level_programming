7a99fb1910b1412faffbf4a16451cbddb4606665
endritNovaku <ennald@outlook.com>
2022-05-23 11:26:12 -0700

    fix

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -14,23 +14,9 @@ class Rectangle:
     print_symbol = "#"
     """create a functions for ractengle"""
     def __init__(self, width=0, height=0):
-        self.height = height
         self.width = width
-        type(self).number_of_instances += 1
-
-    @property
-    def height(self):
-        """get height"""
-        return self.__height
-
-    @height.setter
-    def height(self, height):
-        """set height"""
-        if type(height) != int:
-            raise TypeError("height must be an integer")
-        elif height < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = height
+        self.height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -46,15 +32,30 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         self.__width = width
 
+    @property
+    def height(self):
+        """get height"""
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        """set height"""
+        if type(height) != int:
+            raise TypeError("height must be an integer")
+        elif height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
+
     def area(self):
         """return the area of a rectangle"""
         return self.__height * self.__width
 
     def perimeter(self):
         """return perimeter of a rectangle"""
-        if self.height == 0 or self.width == 0:
-            return 0
-        return 2 * (self.height + self.width)
+         perimeter = 0
+            return perimeter
+        perimeter = 2 * self.__width + 2 * self.__height
+        return perimeter
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
@@ -94,5 +95,5 @@ class Rectangle:
 
     def __del__(self):
         """delete a instance"""
-        type(self).number_of_instances -= 1
+        Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
