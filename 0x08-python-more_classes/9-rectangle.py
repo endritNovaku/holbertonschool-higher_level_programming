eb5db72012a3c20534d8f6b9785519e749524d8a
endritNovaku <ennald@outlook.com>
2022-05-23 12:27:43 -0700

    fix

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -14,8 +14,8 @@ class Rectangle:
     print_symbol = "#"
     """create a functions for ractengle"""
     def __init__(self, width=0, height=0):
-        self.height = height
         self.width = width
+        self.height = height
         type(self).number_of_instances += 1
 
     @property
