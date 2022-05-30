be280ba9aa21c46ab2b7f6f0cdd6dff054899e7f
endritNovaku <ennald@outlook.com>
2022-05-30 13:58:45 -0700

    fix

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
+"""Define Square class"""
 Rectangle = __import__('9-rectangle').Rectangle
-"""Square class"""
 
 
 class Square(Rectangle):
-    """init method"""
+    """Square class"""
+
     def __init__(self, size):
+        """
+        __init - inicialize square class
+        Args:
+            size(int): size of the square
+        """
+
         self.integer_validator("size", size)
         self.__size = size
 
-    """area method"""
     def area(self):
+        """Return the area of the square"""
+
         return self.__size * self.__size
--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,18 +1,27 @@
 #!/usr/bin/python3
-Rectangle = __import__('9-rectangle').Rectangle
 """Square class"""
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """init method"""
+    """Square class"""
+
     def __init__(self, size):
+        """
+        __init__ - inicialize square class
+        Args:
+            size(int): size of the square
+        """
+
         self.integer_validator("size", size)
         self.__size = size
 
-    """area method"""
     def area(self):
+        """Return the square of the area"""
+
         return self.__size * self.__size
 
-    """str method"""
     def __str__(self):
+        """Return as a string the width and height of the square"""
+
         return f'[Square] {self.__size}/{self.__size}'
