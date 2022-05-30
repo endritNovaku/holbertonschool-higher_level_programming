c25cbf27fca16ddb58306643da6ba0711533f26f
endritNovaku <ennald@outlook.com>
2022-05-30 06:35:01 -0700

    fix

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 Rectangle = __import__('9-rectangle').Rectangle
+"""Square class"""
+
 
 class Square(Rectangle):
+    """init method"""
     def __init__(self, size):
         self.integer_validator("size", size)
         self.__size = size
 
-
+    """str method"""
     def __str__(self):
         return f'[Rectangle] {self.__size}/{self.__size}'
     def area(self):
--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""Rectangle class"""
+
 
 class Rectangle(BaseGeometry):
+    """init method"""
     def __init__(self, width, height):
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
         self.__height = height
 
+    """area method"""
     def area(self):
         return self.__width * self.__height
 
+    """str method"""
     def __str__(self):
         return f'[Rectangle] {self.__width}/{self.__height}'
