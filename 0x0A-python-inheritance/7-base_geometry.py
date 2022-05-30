5bb0d9f78c343054145af0a79fdf5bbfce15de34
endritNovaku <ennald@outlook.com>
2022-05-30 13:00:44 -0700

    fix

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
+"""BaseGeometry class"""
+
 
 class BaseGeometry:
+    """area function"""
+
+
     def area(self):
+        """raise an Exception error"""
         raise Exception("area() is not initialized")
 
+    """define integer_validator function"""
+
+
     def integer_validator(self, name, value):
+        """check if a value is int or not"""
         self.name = name
         if type(value) != int:
             raise TypeError(f"{name} must be an integer")
