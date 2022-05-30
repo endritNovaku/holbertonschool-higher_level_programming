3a3b454d6986d2a5254c1adc8180332788390933
endritNovaku <ennald@outlook.com>
2022-05-30 05:27:53 -0700

    8-rectangle.py

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+class BaseGeometry:
+    def area(self):
+        raise Exception("area() is not initialized")
+
+    def integer_validator(self, name, value):
+        if type(value) != int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
