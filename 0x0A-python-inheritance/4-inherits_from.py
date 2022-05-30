09adf086cf6ef86015a7453623c8a5e5649854d5
endritNovaku <ennald@outlook.com>
2022-05-30 12:52:13 -0700

    fix

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""Returns True or False based on if
+the object is an instance of a class"""
+
 
 def inherits_from(obj, a_class):
+    """Return True or False"""
     return issubclass(type(obj), a_class) and type(obj) != a_class
