d116a1db78b64ebd3c4058bb270fac2f0ecb9eaa
endritNovaku <ennald@outlook.com>
2022-05-30 14:00:54 -0700

    fix

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -3,5 +3,11 @@
 
 class MyList(list):
     """print a sorted list"""
+
     def print_sorted(self):
+        """
+        print_sorted - print sorted list
+        Args:
+            self: MyList
+        """
         print(sorted(self))
