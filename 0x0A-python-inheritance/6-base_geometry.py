e769dead4a62d27d77925029d45ed5b6c365b86c
endritNovaku <ennald@outlook.com>
2022-05-30 12:55:41 -0700

    fix

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""class BaseGeometry that raises a Exception error"""
+
 
 class BaseGeometry:
+    """raise Exception error"""
     raise Exception("area() is not implemented")
