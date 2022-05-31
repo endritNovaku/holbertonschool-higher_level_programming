502c4af201464625decf03f5032989f9bd025c88
endritNovaku <ennald@outlook.com>
2022-05-31 11:28:10 -0700

    fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
+"""save all arguments to a json file"""
+
+
 import sys
 import os.path
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 l = []
-
 if os.path.exists("add_item.json"):
     l = load_from_json_file("add_item.json")
 
