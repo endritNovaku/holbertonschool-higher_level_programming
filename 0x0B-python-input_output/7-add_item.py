f4575f6388da88aba0e0ac2340327a884e79fd76
endritNovaku <ennald@outlook.com>
2022-05-31 11:27:08 -0700

    fix

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+import os.path
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+l = []
+
+if os.path.exists("add_item.json"):
+    l = load_from_json_file("add_item.json")
+
+save_to_json_file(l + sys.argv[1:], "add_item.json")
