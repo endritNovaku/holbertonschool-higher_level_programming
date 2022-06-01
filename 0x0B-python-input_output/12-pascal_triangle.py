7cb5f7862a717925dfaefc407d02e3611c344be7
endritNovaku <ennald@outlook.com>
2022-06-01 02:43:11 -0700

    pascal triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""pascal fun"""
+
+def pascal_triangle(n):
+    """create a pascal num"""
+    newArr = []
+    result = []
+    newN = 1
+    if n <= 0:
+        return newArr
+
+    for i in range(n):
+        num = newN
+        newArr.append([])
+        if num > 9:
+            while num > 9:
+                newArr[i].append(int(num % 10))
+                num = num / 10
+        newArr[i].append(int(num))
+        newN = newN * 11
+
+    
+    return newArr
