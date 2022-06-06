a15c25e0479eff0cd06f1858d7d8aedfef8b7ad0
endritNovaku <ennald@outlook.com>
2022-06-06 02:51:37 -0700

    fix

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -21,7 +21,7 @@ class Base:
         """return dictionaries to json string"""
 
         if list_dictionaries is None or list_dictionaries is []:
-            return "[]"
+            return []
         return json.dumps(list_dictionaries)
 
     @staticmethod
