9143c521c0b9aed704f80f32fd7281084ed2779e
endritNovaku <ennald@outlook.com>
2022-06-06 00:33:36 -0700

    fix

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """define base class"""
 import json
+from python-input_output.3-to_json_string import to_json_string
 
 
 class Base:
@@ -22,3 +23,28 @@ class Base:
         if list_dictionaries is None:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """save to file"""
+
+        new_cvs = []
+        new_file = cls.__name__ + ".csv"
+        if new_file is not None:
+            for i in list_objs:
+                new_cvs.append(i.to_dictionary())
+        with open(new_file, "w", encoding="utf-8") as f:
+            f.write(cls.to_json_string(new_list_csv))
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """list of instances"""
+
+        csv_file = cls.__name__ + ".csv"
+        if path.exists(csv_file):
+            with open(csv_file, "r", encoding="utf-8") as file:
+                csv_dict = cls.from_json_string(file.read())
+                csv_list = [cls.create(**csv_inst) for csv_inst in csv_dict]
+                return csv_list
+        else:
+            return []
