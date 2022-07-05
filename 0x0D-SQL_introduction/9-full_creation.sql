7875c7129c78aafe15c140ae8e467dbacfed7eb2
endritNovaku <ennald@outlook.com>
2022-07-05 10:56:08 -0700

    fix

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,7 @@
+-- create a table second_table in the databese hbtn_0c_0
+CREATE TABLE second_table (
+	id INT,
+	name VARCHAR(256),
+	score INT
+);
+INSERT INTO second_table VALUES (1, Jhon, 10), (2, Alex, 3), (3, Bob, 14), (4, George, 8)
