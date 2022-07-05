2c80fdc9ffc050704f4a8fae4b94319114111518
endritNovaku <ennald@outlook.com>
2022-07-05 11:20:06 -0700

    fix

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- list the number of records with the same score
-SELECT score, COUNT(score) number FROM second_score GROUP BY score ORDER BY number DESC
+SELECT score, COUNT(score) number FROM second_score GROUP BY score ORDER BY number DESC;
