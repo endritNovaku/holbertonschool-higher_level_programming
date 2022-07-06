7936cd7fdadb208b79d20e85203b0ad877a13779
endritNovaku <ennald@outlook.com>
2022-07-06 03:46:54 -0700

    fix

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- creates the database hbtn_0d_usa and the tables states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE states (
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
 	id INT MEDIUMINT NOT NULL AUTO_INCREMENT,
 	name VARCHAR(256)
 	PRIMARY KEY (id)
