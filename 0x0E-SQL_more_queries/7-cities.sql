3b9072b249fb8476b498536ca8f8978fd1b02ae2
endritNovaku <ennald@outlook.com>
2022-07-06 04:08:06 -0700

    fix

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -4,5 +4,5 @@ CREATE TABLE IF NOT NULL hbtn_0d_usa.cities (
 	id INT NOT NULL UNIQUE AUTO_INCREMENT,
 	state_id INT NOT NULL,
 	name VARCHAR(256) NOT NULL
-	PRIMARY KEY (id), FOREIGN KEY (id)
+	PRIMARY KEY (id), FOREIGN KEY (state_id)
 	);
