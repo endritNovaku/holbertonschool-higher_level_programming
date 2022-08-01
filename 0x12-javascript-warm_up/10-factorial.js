08f8339d9d0b9c53ef3319feba47c88ba1041b93
endritNovaku <ennald@outlook.com>
2022-08-01 04:59:47 -0700

    fix

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-function factorial(){
-	let result = 1;
-	for (let i = parseInt(process.argv[2]); i > 0; i--) {
-  	result *= i;
-	}
-	return result
+function factorial () {
+  let result = 1;
+  for (let i = parseInt(process.argv[2]); i > 0; i--) {
+    result *= i;
+  }
+  return result;
 }
 console.log(factorial());
