19d376505752b113a2d63ab1e0bed759901b287f
endritNovaku <ennald@outlook.com>
2022-08-01 03:48:18 -0700

    11-second_biggest.js

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+let biggestNum = 0;
+let secondNum = 0;
+for (let i = 2; process.argv[i] != undefined; i++) {
+	if (parseInt(process.argv[i]) > biggestNum) {
+		biggestNum = parseInt(process.argv[i]);
+	}
+}
+
+for (let i = 2; process.argv[i] != undefined; i++) {
+        if (parseInt(process.argv[i]) < biggestNum) {
+                secondNum = parseInt(process.argv[i]);
+        }
+}
+console.log(secondNum);
