8ddf9c5b7a61bed4473b4e6e9c401f22eb88eb97
endritNovaku <ennald@outlook.com>
2022-08-01 04:52:28 -0700

    fix

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 if (process.argv[2] === undefined) {
   console.log('No argument');
-} else if (process.argv[2] !== undefined) {
+} else if (process.argv[2] !== undefined && process.argv[3] === undefined) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
