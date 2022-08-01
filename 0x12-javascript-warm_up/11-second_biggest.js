31d1289066558443de1f3b4de2f6814fe9c162b8
endritNovaku <ennald@outlook.com>
2022-08-01 04:49:00 -0700

    fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,15 +2,13 @@
 let biggestNum = 0;
 let secondNum = 0;
 for (let i = 2; process.argv[i] !== undefined; i++) {
-	if (parseInt(process.argv[i]) > biggestNum) {
-		biggestNum = parseInt(process.argv[i]);
-	}
+  if (parseInt(process.argv[i]) > biggestNum) {
+    biggestNum = parseInt(process.argv[i]);
+  }
 }
-console.log(biggestNum);
-
 for (let i = 2; process.argv[i] !== undefined; i++) {
-	if (parseInt(process.argv[i]) < biggestNum && secondNum < parseInt(process.argv[i])) {
-		secondNum = parseInt(process.argv[i]);
-	}
+  if (parseInt(process.argv[i]) < biggestNum && secondNum < parseInt(process.argv[i])) {
+    secondNum = parseInt(process.argv[i]);
+  }
 }
 console.log(secondNum);
