ba61d8084e498ad41c1387f6d05700ab14986c79
endritNovaku <ennald@outlook.com>
2022-08-01 04:51:12 -0700

    fix

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
 if (process.argv[2] === undefined) {
   console.log('No argument');
-} else {
+} else if (process.argv[2] !== undefined) {
   console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
