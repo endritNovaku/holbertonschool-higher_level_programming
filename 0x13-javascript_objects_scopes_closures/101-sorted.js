6d2f14fa43d53f2f0da7a5cc7d68105ce7e75798
endritNovaku <ennald@outlook.com>
2022-08-02 08:19:22 -0700

    fix

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -2,13 +2,13 @@
 const dict = require('./101-data').dict;
 const newDict = {};
 for (const key in dict) {
-  if (!Object.prototype.hasOwnProperty.call(dict, dict[key])) {
+  if (!dict.hasOwnProperty(dict[key])) {
     newDict[dict[key]] = [];
   }
 }
 
 for (const key in dict) {
-  if (Object.prototype.hasOwnProperty.call(dict, key)) {
+  if (dict.hasOwnProperty.call(dict[key])) {
     newDict[dict[key]].push(key);
   }
 }
