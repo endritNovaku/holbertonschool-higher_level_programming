5f197a0db24e09d25b8bb3f9f632d1ff112289ca
endritNovaku <ennald@outlook.com>
2022-08-06 08:52:00 -0700

    fix

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+"""print states from the hbtn_0e_0_usa"""
 import MySQLdb
 import sys
 
-"""print states from the hbtn_0e_0_usa"""
 
 if __name__ == "__main__":
     db = MySQLdb.connect(
@@ -10,9 +10,15 @@ if __name__ == "__main__":
             port=3306,
             user=sys.argv[1],
             passwd=sys.argv[2],
-            db=sys.argv[3])
+            db=sys.argv[3],
+            charset="utf8")
     cur = db.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id=states.id ORDER BY cities.id")
+    query = " ".join(["SELECT cities.id cities.name, states.name",
+                      "FROM cities INNER JOIN states ON cities.state_id=states.id",
+                      "ORDER BY cities.id"])
+    cur.execute(query)
     rows = cur.fetchall()
     for row in rows:
         print(row)
+    cur.close()
+    db.close()
