187dc36a89b7e04ea9b6325661de2e4fbe064b7d
endritNovaku <ennald@outlook.com>
2022-08-06 10:18:34 -0700

    fix

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -3,8 +3,6 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey
 
-Base = declarative_base()
-
 
 class City(Base):
     __tablename__ = 'cities'
