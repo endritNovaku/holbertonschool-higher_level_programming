7e166e00c503599267f1ec959301c73ca459ebc7
endritNovaku <ennald@outlook.com>
2022-08-06 09:36:49 -0700

    fix

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -5,12 +5,15 @@ from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
+
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-            .format(sys.argv[1], sys.argv[2], sys.argv[3]),pool_pre_ping=True)
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]),pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
     startsWith = session.query(State).filter(State.name.contains("a")).all()
     for i in startsWith:
         print(f"{i.id}: {i.name}")
+    session.close()
