fb245a44ef2fe133607877988b37d8896bd34940
endritNovaku <ennald@outlook.com>
2022-08-06 09:48:07 -0700

    fix

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -9,12 +9,12 @@ from model_state import Base, State
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2],
-                                   sys.argv[3]),pool_pre_ping=True)
+                                   sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
     startsWith = session.query(State).filter_by(name=sys.argv[4]).first()
     if startsWith:
-        print(f"{startsWith.id}: {startsWith.name}")
+        print(f"{startsWith.id}")
     else:
         print("Not found")
