170bf66a965db5ec54d2be3e20bcf3cdb71bf3b3
endritNovaku <ennald@outlook.com>
2022-08-18 06:02:53 -0700

    fix

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sent a get request to the url and display the body of the response
-curl -sX get -h "X-HolbertonSchool-User-Id: 98" "$1"
+curl -sX GET -H "X-HolbertonSchool-User-Id: 98" "$1"
