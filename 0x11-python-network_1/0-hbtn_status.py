572b8a5d714b64d4799164776fea9d3f43213122
endritNovaku <ennald@outlook.com>
2022-08-21 12:37:59 -0700

    fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-import urllib.request
-
 """status for intranet.hbtn.io/status/"""
+import urllib.request
 if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status/') as res:
         content = res.read()
