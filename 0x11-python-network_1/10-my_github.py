dc9fae2bba1cebef1bdc9c2db5261b07975ed945
endritNovaku <ennald@outlook.com>
2022-08-21 14:50:21 -0700

    fix

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -4,6 +4,6 @@ import requests
 from sys import argv
 if __name__ == "__main__":
     res = requests.get('https://api.github.com/user',
-                            auth=(argv[1], argv[2]))
+                       auth=(argv[1], argv[2]))
     data = res.json()
     print(data.get('id'))
