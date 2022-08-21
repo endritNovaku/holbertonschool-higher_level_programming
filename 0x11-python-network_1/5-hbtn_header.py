ae1e2db3caef577306f41d9a3d64fa0eff0410df
endritNovaku <ennald@outlook.com>
2022-08-21 13:57:49 -0700

    fix

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 'fetch https://intranet.hbtn.io/status using requests library'
 import requests
+from sys import argv
 
-r = requests.get('https://intranet.hbtn.io/status')
+r = requests.get(argv[1])
 print(r.headers['X-Request-Id'])
