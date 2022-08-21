8605deef901fb988d2f9e4ecf810c21ea1b299cc
endritNovaku <ennald@outlook.com>
2022-08-21 13:17:38 -0700

    fixing pycodestyle

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     url = sys.argv[1]
     values = {'email': sys.argv[2]}
     data = urllib.parse.urlencode(values)
-    data = data.encode('ascii') # data should be bytes
+    data = data.encode('ascii')
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
         the_page = response.read()
