136c4bef8b8c13d5eadba06e63207ef3297758d4
endritNovaku <ennald@outlook.com>
2022-08-21 13:25:09 -0700

    fix

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """status for intranet.hbtn.io/status/"""
 import urllib.request
-import urlib.parse
-import sys
+import urllib.parse
+from sys import argv
 if __name__ == "__main__":
-    url = sys.argv[1]
-    values = {'email': sys.argv[2]}
+    values = {'email': argv[2]}
     data = urllib.parse.urlencode(values)
     data = data.encode('ascii')
-    req = urllib.request.Request(url, data)
+    req = urllib.request.Request(argv[1], data)
     with urllib.request.urlopen(req) as response:
-        print(response.read().decode("UTF-8")
+        print(response.read().decode("UTF-8"))
