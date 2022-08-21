a344bbd0affcb76fef3e05999102dbe27b88fa1f
endritNovaku <ennald@outlook.com>
2022-08-21 14:47:34 -0700

    fix

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""login to github acc"""
+import requests
+from sys import argv
+if __name__ == "__main__":
+    res = requests.get('https://api.github.com/user',
+                            auth=(argv[1], argv[2]))
+    data = response.json()
+    print(data.get('id'))
