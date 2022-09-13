91535d6b75f68a0f8d87b76aca262d218db13f77
endritNovaku <ennald@outlook.com>
2022-09-13 09:04:34 -0700

    3-starwars_title.js

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const axios = require('axios');
+
+axios.get('https://swapi-api.hbtn.io/api/films/' + process.argv[2])
+  .then(response => {
+    console.log(response.data.title);
+  })
+  .catch(error => {
+    console.log(error);
+  });
