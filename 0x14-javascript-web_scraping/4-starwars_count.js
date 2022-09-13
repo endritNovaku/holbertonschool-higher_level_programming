240f521f646a7c703787a7dbc6430d8cd7573c6d
endritNovaku <ennald@outlook.com>
2022-09-13 09:28:23 -0700

    fixing semistandard

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,20 +4,17 @@ const axios = require('axios');
 
 axios.get(process.argv[2])
   .then(response => {
-	const movies = response.data.results;
-	const actorAdress = "https://swapi-api.hbtn.io/api/people/18/"
-	let actorShowen = 0;
-	for (let i = 0; i < movies.length; i++)
-	{
-		for (let j = 0; j < movies[i].characters.length; j++)
-		{
-			if (movies[i].characters[j] == actorAdress)
-			{
-				actorShowen++;
-			}
-		}
-	}
-	console.log(actorShowen);
+    const movies = response.data.results;
+    const actorAdress = 'https://swapi-api.hbtn.io/api/people/18/';
+    let actorShowen = 0;
+    for (let i = 0; i < movies.length; i++) {
+      for (let j = 0; j < movies[i].characters.length; j++) {
+        if (movies[i].characters[j] === actorAdress) {
+          actorShowen++;
+        }
+      }
+    }
+    console.log(actorShowen);
   })
   .catch(error => {
     console.log(error);
