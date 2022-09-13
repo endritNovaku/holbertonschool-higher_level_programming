dff35f9ce1fef50bc8c27c14956f373014a5d300
endritNovaku <ennald@outlook.com>
2022-09-13 09:01:55 -0700

    fix

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-const axios = require('axios').default;
+const axios = require('axios');
 
 axios.get(process.argv[2])
   .then(res => {
-    console.log('code:', res.status);
-  });
-  .catch(function (error) {
-    console.log('code:', error.response.status);
+    console.log('code: %s', res.status);
+  })
+  .catch(error => {
+    console.log('code: %s', error.response.status);
   });
